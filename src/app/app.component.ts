d4f3b02b2eaff2ae3318e9c9e715cd62efef5266
esedshaba <esedsh@gmail.com>
2025-11-14 07:48:08 +0100

    Added csv data needed Added svgs for the map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-  imports: [],
+    imports: [],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
