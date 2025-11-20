03611b38f4b5e93c3caad7d8be158010d1045478
esedshaba <esedsh@gmail.com>
2025-11-20 08:04:00 +0100

    Finished header with working language and translation service

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
 import {TranslateService} from '../services/translate.service';
+import {Language, LanguageService} from '../services/language.service';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  constructor(private translateService: TranslateService) {
+  constructor(private translateService: TranslateService,
+              private languageService: LanguageService) {
+  }
+
+  changeLanguage(lang: Language){
+    this.languageService.setCurrentLanguage(lang)
+  }
+
+  currentLanguage(){
+    return this.languageService.getCurrentLanguage();
   }
 
   __(key: string): string {
     return this.translateService.translate('header.' + key);
   }
+
+  protected readonly Language = Language;
 }
--- src/app/services/translate.service.ts
+++ src/app/services/translate.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@angular/core';
 import {Language, LanguageService} from './language.service';
 
 const TRANSLATIONS : { [key: string]: Record<Language, string> } = {
-  'header.test': {
-    'sq': 'KONTAKTO',
-    'en': 'CONTACT'
+  'header.description': {
+    'sq': 'Rezultatet e zgjedhjeve vendore të 2023 dhe të zgjedhje parlamentare të 2025 në bashkinë e Tiranës sipas ' +
+          'vendndodhjes së qendrave të votimit.',
+    'en': 'Results of the 2023 local elections and the 2025 parliamentary elections in the municipality of Tirana ' +
+          'according to the location of the polling stations.'
   },
 }
 
