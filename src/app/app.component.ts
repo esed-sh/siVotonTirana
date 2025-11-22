22121b2aa000b59e23c28379f191f3290bd40d35
esedshaba <esedsh@gmail.com>
2025-11-22 16:59:48 +0100

    Added (empty) footer Tried to make touch easier on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {MainContentComponent} from './main-content/main-content.component';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
   imports: [
     HeaderComponent,
-    MainContentComponent
+    MainContentComponent,
+    FooterComponent
   ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
